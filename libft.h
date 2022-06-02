ba578ef76ca53f223e2b71d13548cc323ca86147
Carlos Rocha <carlosrocha@MacBook-Pro-de-Carlos.local>
2022-06-02 18:46:01 -0300

    Add hedder "libft.h"

---
+++ libft.h
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caalbert <caalbert@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/02 16:36:07 by caalbert          #+#    #+#             */
+/*   Updated: 2022/06/02 16:52:29 by caalbert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/**
+ * @brief Verifies if the param c is alphajbetic
+ * 
+ * @param c  Character to be analysed.
+ * @return int Return true if c is alphabetical, else returns false.
+ */
+int ft_isapha(int c);
+
+/**
+ * @brief Verifies if the params 
+ * 
+ * @return int 
+ */
+int ft_isdigit();
+
+int ft_isalnum();
+
+int ft_isascii();
+
+int ft_isprint();
+
+int ft_strlen();
+
+int ft_memset();
+
+int ft_bzero();
+
+int ft_memcpy();
+
+int ft_memmove();
+
+int ft_strlcpy();
+
+int ft_strlcat();
+
+int ft_toupper();
+
+int ft_tolower();
+
+int ft_strchr();
+
+int ft_strrchr();
+
+int ft_strncmp();
+
+int ft_memchr();
+
+int ft_memcmp();
+
+int ft_strnstr();
+
+int ft_atoi();
+
+
