0585cd030c55eed35eb6005c3f1980e657c3e824
caalbert <caalbert@student.42sp.org.br>
2022-07-27 17:07:55 +0200

    feat: insert badge and score

--- README.md
+++ README.md
@@ -1,25 +1,34 @@
-# LIBFT (by Carlos Rocha)
+<div align="center">
 
-## Index Functions
+![ft_printfn](https://user-images.githubusercontent.com/3737837/179274741-9122c328-d18f-4501-bda8-1c48098a60b8.png)
+
+# libft
+
+
+[![caalbert's 42 Libft Score](https://badge42.vercel.app/api/v2/cl5g4kxwa002509l0nrey4oz0/project/2606362)](https://github.com/JaeSeoKim/badge42)
+
+</div>
+
+## Index Functions done
 
 - [x] isalpha
 - [x] isdigit
 - [x] isalnum
-- [ ] isascii
-- [ ] isprint
-- [ ] strlen
-- [ ] memset
-- [ ] bzero
-- [ ] memcpy
-- [ ] memmove
-- [ ] strlcpy
-- [ ] strlcat
-- [ ] toupper
-- [ ] tolower
-- [ ] strchr
-- [ ] strrchr
-- [ ] strncmp
-- [ ] memchr
-- [ ] memcmp
-- [ ] strnstr
-- [ ] atoi
+- [x] isascii
+- [x] isprint
+- [x] strlen
+- [x] memset
+- [x] bzero
+- [x] memcpy
+- [x] memmove
+- [x] strlcpy
+- [x] strlcat
+- [x] toupper
+- [x] tolower
+- [x] strchr
+- [x] strrchr
+- [x] strncmp
+- [x] memchr
+- [x] memcmp
+- [x] strnstr
+- [x] atoi
--- libft.h
+++ libft.h
@@ -5,60 +5,64 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: caalbert <caalbert@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/02 16:36:07 by caalbert          #+#    #+#             */
-/*   Updated: 2022/06/02 19:30:28 by caalbert         ###   ########.fr       */
+/*   Created: 2022/06/13 04:39:39 by caalbert          #+#    #+#             */
+/*   Updated: 2022/06/28 19:25:35 by caalbert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/**
- * @brief Verifies if the param c is alphajbetic
- * 
- * @param c  Character to be analysed.
- * @return int Return true if c is alphabetical, else returns false.
- */
-int ft_isapha(int c);
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <unistd.h>
+# include <stdlib.h>
 
-/**
- * @brief Verifies if the params 
- * 
- * @return int 
- */
-int ft_isdigit(int d);
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
 
-int ft_isalnum();
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_atoi(const char *nptr);
+void	ft_bzero(void *s, size_t n);
+int		ft_isdigit(int c);
+int		ft_isalpha(int c);
+int		ft_isalnum(int c);
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
+char	*ft_strnstr(const char *str, const char *little, size_t len);
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+void	*ft_memset(void *s, int c, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+void	*ft_calloc(size_t nmemb, size_t size);
+char	*ft_itoa(int n);
+char	*ft_strdup(const char *s);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(char const *s1, char const *set);
+char	**ft_split(char const *s, char c);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
-int ft_isascii();
-
-int ft_isprint();
-
-int ft_strlen();
-
-int ft_memset();
-
-int ft_bzero();
-
-int ft_memcpy();
-
-int ft_memmove();
-
-int ft_strlcpy();
-
-int ft_strlcat();
-
-int ft_toupper();
-
-int ft_tolower();
-
-int ft_strchr();
-
-int ft_strrchr();
-
-int ft_strncmp();
-
-int ft_memchr();
-
-int ft_memcmp();
-
-int ft_strnstr();
-
-int ft_atoi();
+#endif
