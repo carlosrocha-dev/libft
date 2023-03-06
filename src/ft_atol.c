edd9cd2a00c292cd4cc273f1d949ac20c04e4da1
Carlos Rocha <carlos.rocha01@gmail.com>
2023-03-06 09:32:53 -0300

    feature: include new function ft_atol.c

---
+++ src/ft_atol.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: caalbert <caalbert@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/06 09:31:27 by caalbert          #+#    #+#             */
+/*   Updated: 2023/03/06 09:31:28 by caalbert         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+long	ft_atol(const char *nptr)
+{
+	size_t			i;
+	unsigned long	nbr;
+	int				signe;
+
+	i = 0;
+	nbr = 0;
+	signe = 1;
+	while (ft_isspace(nptr[i]))
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		if (nptr[i] == '-')
+			signe = -1;
+		i++;
+	}
+	while (nptr[i] == '0')
+		i++;
+	while (ft_isdigit(nptr[i]))
+	{
+		nbr = nbr * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return (signe * nbr);
+}
